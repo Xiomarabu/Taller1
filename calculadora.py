25767304003a3cd6d7f79e29ab6f839e6ee0de4c
Xiomara1 <xbustos201@unab.edu.co>
2023-08-12 11:56:17 -0500

    creacion de suma

---
+++ calculadora.py
@@ -0,0 +1,37 @@
+print("Hola, bienvenido a la calculadora")
+numero1 =  int(input("inserte primer numero: "))
+numero2 =  int(input("inserte segundo numero: "))
+operacion = input("inserta simbolo de operacion(solo puedes insertar +,-,*,/)")
+
+if operacion == "+":
+    resultado = numero1 + numero2
+    print("el resultado de su suma es:", resultado)
+
+elif operacion == "-":
+    resultado = numero1 - numero2
+    print("el resultado de su resta es ", resultado)
+
+elif operacion == "*":
+    resultado = numero1 * numero2
+    print("el restultado de tu multiplacion es ", resultado)
+
+elif operacion == "/":
+    resultado = numero1 / numero2
+    print("el resultado de la division es ", resultado)
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
